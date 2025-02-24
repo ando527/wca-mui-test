014975c0f50859ca85097da7673f567d2377d403
Gregor Billing <gregor.billing.dev@gmail.com>
2025-02-24 15:17:07 +0900

    Use VStack and HStack

--- src/app/information/styleguide/page.tsx
+++ src/app/information/styleguide/page.tsx
@@ -1,38 +1,38 @@
 import * as React from 'react';
-import {Button, Center, Heading, Flex, Stack, Text, For} from "@chakra-ui/react";
+import {Button, Heading, Text, For, HStack, Container, VStack} from "@chakra-ui/react";
 
 const buttonVariants = ['solid', 'ghost', 'outline', 'plain', 'subtle'];
 const wcaColors = ["wcablue", "wcagreen", "wcared", "wcaorange", "wcayellow"];
 
 export default function Home() {
   return (
-    <Center>
-      <Stack maxW="2xl">
-        {/* Page Title */}
-        <Heading>
-          STYLE GUIDE
-        </Heading>
+    <Container maxWidth="2xl">
+      {/* Page Title */}
+      <Heading>
+        STYLE GUIDE
+      </Heading>
+      <VStack>
         <For each={buttonVariants}>
           {(buttonVariant, index) => (
-              <React.Fragment key={`button-variant-${index}`}>
-                <Text>Buttons: {buttonVariant}</Text>
-                <Flex gap="2">
-                  <For each={wcaColors} >
-                    {(wcaColor, index) => (
-                      <Button
-                        key={`button-variant-${buttonVariant}-${index}`}
-                        variant={buttonVariant}
-                        colorPalette={wcaColor}
-                      >
-                        {wcaColor.replace("wca", "").toLowerCase()}
-                      </Button>
-                    )}
-                  </For>
-                </Flex>
-              </React.Fragment>
+            <React.Fragment key={`button-variant-${index}`}>
+              <Text>Buttons: {buttonVariant}</Text>
+              <HStack justify="space-between">
+                <For each={wcaColors} >
+                  {(wcaColor, index) => (
+                    <Button
+                      key={`button-variant-${buttonVariant}-${index}`}
+                      variant={buttonVariant}
+                      colorPalette={wcaColor}
+                    >
+                      {wcaColor.replace("wca", "").toLowerCase()}
+                    </Button>
+                  )}
+                </For>
+              </HStack>
+            </React.Fragment>
           )}
         </For>
-      </Stack>
-    </Center>
+      </VStack>
+    </Container>
     );
 }
