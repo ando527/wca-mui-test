8290a6b3b2295e53f5b239ad5d2375c366d53baa
ando527 <43904.ando527@gmail.com>
2025-03-10 10:18:29 +1000

    updated styling for results table

--- src/components/PersonalRecordsTable.tsx
+++ src/components/PersonalRecordsTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Card, Table, Flex, Icon} from "@chakra-ui/react";
+import {Card, Table, Flex, Icon, Text, Box} from "@chakra-ui/react";
 import { Tooltip } from "@/components/ui/tooltip";
+import { LuBox } from "react-icons/lu";
 
 import "@cubing/icons/css";
 
@@ -51,7 +52,7 @@ const PersonalRecordsTable: React.FC<RecordsProps> = ({ records }) => {
         <Table.Root size="sm" striped rounded="md" variant="results">
           <Table.Header>
             <Table.Row bg="bg.inverted">
-              <Table.ColumnHeader color="fg.inverted">Event</Table.ColumnHeader>
+              <Table.ColumnHeader color="fg.inverted" pl="3"><Flex direction="row"><Box fontSize="18px"><LuBox/></Box><Text fontWeight="medium">Event</Text></Flex></Table.ColumnHeader>
               <Tooltip content="National Ranking" showArrow openDelay={100}>
                 <Table.ColumnHeader color="fg.inverted" textAlign="right">NR</Table.ColumnHeader>
               </Tooltip>
@@ -77,34 +78,34 @@ const PersonalRecordsTable: React.FC<RecordsProps> = ({ records }) => {
           <Table.Body>
             {records.map((record, index) => (
                 <Table.Row key={index} bg="bg.inverted">
-                  <Table.Cell color="fg.inverted">
+                  <Table.Cell color="fg.inverted" pl="3">
                     <Flex direction="row">
                       <Icon width={18} height={18} fontSize="md">
                         <i className={`cubing-icon event-${record.event}`} />
                       </Icon>
-                      {eventMap[record.event]}
+                      <Text fontWeight="medium">{eventMap[record.event]}</Text>
                     </Flex>
                   </Table.Cell>
-                  <Table.Cell color={record.snr < 11 ? "recordColors.national" : "fg.inverted" } fontWeight={record.snr < 11 ? "600" : "400"} textAlign="right">{record.snr}</Table.Cell>
-                  <Table.Cell color={record.scr < 11 ? "recordColors.national" : "fg.inverted"} fontWeight={record.scr < 11 ? "600" : "400"} textAlign="right">{record.scr}</Table.Cell>
-                  <Table.Cell color={record.swr < 11 ? "recordColors.national" : "fg.inverted"} fontWeight={record.swr < 11 ? "600" : "400"} textAlign="right">{record.swr}</Table.Cell>
-                  <Table.Cell color="fg.inverted" fontWeight={600} textAlign="right">{record.single}</Table.Cell>
-                  <Table.Cell color="fg.inverted" fontWeight={600}>{record.average !== 0 ? record.average : ""}</Table.Cell>
+                  <Table.Cell color={record.snr < 11 ? "recordColors.national" : "fg.inverted" } fontWeight={record.snr < 11 ? "bold" : "light"} textAlign="right">{record.snr}</Table.Cell>
+                  <Table.Cell color={record.scr < 11 ? "recordColors.national" : "fg.inverted"} fontWeight={record.scr < 11 ? "bold" : "light"} textAlign="right">{record.scr}</Table.Cell>
+                  <Table.Cell color={record.swr < 11 ? "recordColors.national" : "fg.inverted"} fontWeight={record.swr < 11 ? "bold" : "light"} textAlign="right">{record.swr}</Table.Cell>
+                  <Table.Cell color="fg.inverted" fontWeight="medium" textAlign="right">{record.single}</Table.Cell>
+                  <Table.Cell color="fg.inverted" fontWeight="medium">{record.average !== 0 ? record.average : ""}</Table.Cell>
                   <Table.Cell
                     color={record.awr !== 0 && record.awr < 11 ? "recordColors.national" : "fg.inverted"}
-                    fontWeight={record.awr !== 0 && record.awr < 11 ? "600" : "400"}
+                    fontWeight={record.awr !== 0 && record.awr < 11 ? "bold" : "light"}
                   >
                     {record.awr !== 0 ? record.awr : ""}
                   </Table.Cell>                   
                   <Table.Cell
                     color={record.acr !== 0 && record.acr < 11 ? "recordColors.national" : "fg.inverted"}
-                    fontWeight={record.acr !== 0 && record.acr < 11 ? "600" : "400"}
+                    fontWeight={record.acr !== 0 && record.acr < 11 ? "bold" : "light"}
                   >
                     {record.acr !== 0 ? record.acr : ""}
                   </Table.Cell> 
                   <Table.Cell
                     color={record.anr !== 0 && record.anr < 11 ? "recordColors.national" : "fg.inverted"}
-                    fontWeight={record.anr !== 0 && record.anr < 11 ? "600" : "400"}
+                    fontWeight={record.anr !== 0 && record.anr < 11 ? "bold" : "light"}
                   >
                     {record.anr !== 0 ? record.anr : ""}
                   </Table.Cell>            
