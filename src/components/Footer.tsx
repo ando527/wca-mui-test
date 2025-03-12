f77cd8729720f3ad4e4de164be2b607fbe50d664
ando527 <43904.ando527@gmail.com>
2025-03-12 12:19:46 +1000

    Modified Links to align with requirements for Routing Library

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,10 @@ import {
   IconButton,
   Button,
   Image,
-  Link,
+  Link as ChakraLink,
   VStack
 } from "@chakra-ui/react";
+import Link from "next/link";
 import {LuFacebook, LuGithub, LuInstagram, LuTwitter, LuYoutube} from "react-icons/lu";
 
 export default function Footer() {
@@ -18,17 +19,18 @@ export default function Footer() {
       <VStack>
         {/* Social Media Icons */}
         <HStack>
-
-            <Link
-                href="https://instagram.com"
-                target="_blank"
-                variant="plainLink"
-            >
-              <IconButton variant="ghost">
-                <LuInstagram/>
-              </IconButton>
-            </Link>
-
+            <ChakraLink asChild variant="plainLink">
+              <Link
+                  href="https://instagram.com"
+                  target="_blank"
+                  
+              >
+                <IconButton variant="ghost">
+                  <LuInstagram/>
+                </IconButton>
+              </Link>
+            </ChakraLink>
+            <ChakraLink asChild variant="plainLink">
             <Link
                 href="https://facebook.com"
                 target="_blank"
@@ -38,6 +40,8 @@ export default function Footer() {
                 <LuFacebook/>
               </IconButton>
             </Link>
+            </ChakraLink>
+            <ChakraLink asChild variant="plainLink">
             <Link
                 href="https://twitter.com"
                 target="_blank"
@@ -47,6 +51,8 @@ export default function Footer() {
                 <LuTwitter/>
               </IconButton>
             </Link>
+            </ChakraLink>
+            <ChakraLink asChild variant="plainLink">
             <Link
                 href="https://github.com"
                 target="_blank"
@@ -56,6 +62,8 @@ export default function Footer() {
                 <LuGithub/>
               </IconButton>
             </Link>
+            </ChakraLink>
+            <ChakraLink asChild variant="plainLink">
             <Link
                 href="https://youtube.com"
                 target="_blank"
@@ -65,36 +73,46 @@ export default function Footer() {
                 <LuYoutube/>
               </IconButton>
             </Link>
+            </ChakraLink>
         </HStack>
 
         {/* Footer Links */}
         <HStack gap={5}>
-          <Link href="/">
-            <Image src="/static/images/logo.png" alt="WCA Logo" height={50} width={50}/>          
-          </Link>
-          <Button asChild variant="ghost" size="sm" fontSize="md">
-            <Link href="/about" variant="plainLink">About Us</Link>
-          </Button>
-          <Button asChild variant="ghost" size="sm" fontSize="md">
-            <Link href="/faqs" variant="plainLink">FAQs</Link>
-          </Button>
-          <Button asChild variant="ghost" size="sm" fontSize="md">
-            <Link href="/contact" variant="plainLink">Contact</Link>
-          </Button>
-          <Button asChild variant="ghost" size="sm" fontSize="md">
-            <Link
-              href="https://github.com"
-              target="_blank"
-              variant="plainLink"
-            >
-              GitHub
+          <ChakraLink asChild variant="plainLink">
+            <Link href="/">
+              <Image src="/static/images/logo.png" alt="WCA Logo" height={50} width={50}/>          
             </Link>
+          </ChakraLink>
+          
+          <Button asChild variant="ghost" size="sm" fontSize="md">
+            <ChakraLink asChild variant="plainLink">
+              <Link href="/about">About Us</Link>
+            </ChakraLink>
           </Button>
           <Button asChild variant="ghost" size="sm" fontSize="md">
-            <Link href="/privacy" variant="plainLink">Privacy</Link>
+            <ChakraLink asChild variant="plainLink">
+              <Link href="/faqs">FAQs</Link>
+            </ChakraLink>
           </Button>
           <Button asChild variant="ghost" size="sm" fontSize="md">
-            <Link href="/disclaimer" variant="plainLink">Disclaimer</Link>
+            <ChakraLink asChild variant="plainLink">
+              <Link href="/contact">Contact</Link>
+            </ChakraLink>
+          </Button>
+          <Button asChild variant="ghost" size="sm" fontSize="md">
+            <ChakraLink asChild variant="plainLink">
+              <Link href="https://github.com" target="_blank">GitHub</Link>
+            </ChakraLink>
+          </Button>
+          <Button asChild variant="ghost" size="sm" fontSize="md">
+            <ChakraLink asChild variant="plainLink">
+              <Link href="/privacy">Privacy</Link>
+            </ChakraLink>
+          </Button>
+          <Button asChild variant="ghost" size="sm" fontSize="md">
+            <ChakraLink asChild variant="plainLink">
+              <Link href="/disclaimer">Disclaimer</Link>
+            </ChakraLink>
           </Button>
         </HStack>
       </VStack>
